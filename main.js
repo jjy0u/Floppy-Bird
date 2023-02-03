9b3c77fe4a3e858e36d32b436a0d8f7db5bf644b
Jina <jinayoussef@hotmail.co.uk>
2023-02-03 01:19:08 +0000

    first pipe collision working

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const flappy = document.querySelector("#flappy")
 const game = document.querySelector(".game")
 
 
-const pipes = document.querySelector(".pipes")
 const pipes1 = document.querySelector(".pipes1")
 const pipe1_1 = document.querySelector(".pipes1__pipe1")
 const pipe1_2 = document.querySelector(".pipes1__pipe2")
@@ -11,6 +10,8 @@ const pipes1_hole = document.querySelector(".pipes1__hole")
 const pipes2 = document.querySelector(".pipes2")
 const pipe2_1 = document.querySelector(".pipes2__pipe1")
 const pipe2_2 = document.querySelector(".pipes2__pipe2")
+const pipes2_hole = document.querySelector(".pipes2__hole")
+
 
 let hasGravity = false
 
@@ -39,7 +40,7 @@ const setGravity = () => {
     let gravityAdd = 2
     const frame = () => {
         let flappyTop = parseInt(window.getComputedStyle(flappy).getPropertyValue("top"))
-        if (flappy.style.top == 600 + "px") {
+        if (flappy.style.top == 600 + "px" || (flappy.style.right == pipes1.style.right && flappy.style.top != pipes1_hole.style.top)) {
             clearInterval(gravity);
             pipes1.style.right = 0 + "px";
             pipes2.style.right = -300 + "px";
@@ -59,11 +60,16 @@ const setGravity = () => {
 
 
 const pipeMovement = () => {
-    flappyTouchesPipe()
     let motion = 1
 const frame = () => {
+    const holePosition = pipes1_hole.getBoundingClientRect()
     let pipesRight = parseInt(window.getComputedStyle(pipes1).getPropertyValue("right"))
-    if (flappy.style.top == 600 + "px") {
+    let pipesWidth = parseInt(window.getComputedStyle(pipes1).getPropertyValue("width"))
+    let flappyRight = parseInt(window.getComputedStyle(flappy).getPropertyValue("right"))
+    let flappyTop = parseInt(window.getComputedStyle(flappy).getPropertyValue("top"))
+    let flappyHeight = parseInt(window.getComputedStyle(flappy).getPropertyValue("height"))
+    
+    if (flappy.style.top == 600 + "px" || (((pipes1.style.right <= flappyRight + "px") && (pipesRight + pipesWidth + "px" >= flappyRight + "px") && (pipes1.style.right)>0 + "px") && ((holePosition.top + "px" >= flappy.style.top) || (holePosition.top + holePosition.height + "px" <= flappyTop + flappyHeight + "px")))) {
         clearInterval(movingPipes);
     }
     if (pipes1.style.right == 500 + 'px') {
@@ -80,8 +86,14 @@ const movingPipes = setInterval(frame, 10);
 const pipe2Movement = () => {
     let motion = 1
 const frame = () => {
+    const holePosition = pipes2_hole.getBoundingClientRect()
     let pipesRight = parseInt(window.getComputedStyle(pipes2).getPropertyValue("right"))
-    if (flappy.style.top == 600 + "px") {
+    let pipesWidth = parseInt(window.getComputedStyle(pipes2).getPropertyValue("width"))
+    let flappyRight = parseInt(window.getComputedStyle(flappy).getPropertyValue("right"))
+    let flappyTop = parseInt(window.getComputedStyle(flappy).getPropertyValue("top"))
+    let flappyHeight = parseInt(window.getComputedStyle(flappy).getPropertyValue("height"))
+    
+    if (flappy.style.top == 600 + "px" || (((pipes2.style.right <= flappyRight + "px") && (pipesRight + pipesWidth + "px" >= flappyRight + "px") && (pipes2.style.right)>0 + "px") && ((holePosition.top + "px" >= flappy.style.top) || (holePosition.top + holePosition.height + "px" <= flappyTop + flappyHeight + "px")))) {
         clearInterval(movingPipes);
     }
     if (pipes2.style.right == 500 + 'px') {
@@ -96,27 +108,8 @@ const movingPipes = setInterval(frame, 10);
 }
 
 const randomizePipe = (pipe) => {
-    pipe.style.flex = Math.random() * (5 - 0.3) + 0.3
+    pipe.style.flex = (Math.random() * (5 - 0.3) + 0.3).toFixed(2)
 }
-
-
-const flappyTouchesPipe = () => {
-    let pipesRight = parseInt(window.getComputedStyle(pipes1).getPropertyValue("right"))
-    let flappyRight = parseInt(window.getComputedStyle(flappy).getPropertyValue("right"))
-    if (flappy.style.right == pipes1.style.right && flappy.style.top != pipes1_hole.style.top) {
-        console.log("hello")
-            pipes1.style.right = 0 + "px";
-            pipes2.style.right = -300 + "px";
-            randomizePipe(pipe1_1)
-            randomizePipe(pipe1_2) 
-            randomizePipe(pipe2_1)
-            randomizePipe(pipe2_2) 
-            flappy.style.top = 330 + "px";
-    }
-
-}
-
-flappyTouchesPipe()
 
 
 
